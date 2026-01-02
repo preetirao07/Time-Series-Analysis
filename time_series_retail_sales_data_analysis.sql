26100f32719fd806058dbaa0026fce86019af767
Preeti Yadav <preetirao0711@gmail.com>
2026-01-02 15:54:45 +0530

    Add files via upload

---
+++ time_series_retail_sales_data_analysis.sql
@@ -0,0 +1,47 @@
+TIME SERIES ANALYSIS 
+
+-- Date math
+SELECT date('2020-06-30') - date('2020-05-31') as days;
+
+SELECT age(date('2020-06-30'),date('2020-01-01'));
+
+SELECT date_part('month',age('2020-06-30','2020-01-01')) as months;
+
+SELECT date('2020-06-01') + interval '10 days' as new_date;
+
+SELECT date('2020-06-01') + 17 as new_date;
+
+-- Time math
+
+SELECT time '05:00' + interval '3 hours' as new_time;
+
+We can subtract intervals from times:
+SELECT time '05:00' - interval '3 hours' as new_time;
+
+
+-- The Retail Sales Data Set
+
+-- create table to import data
+create table retail_sales
+(sales_month date,
+naics_code varchar,
+kind_of_business varchar,
+reason_for_null varchar,
+sales decimal);
+
+drop table retail_sales
+-- populate the table with data from csv file through import/export function
+
+select * from retail_sales
+
+1. TRENDING THE DATA
+-- Check the trend of the total retail and food services sales in the US
+select sales_month, sales from retail_sales
+where kind_of_business = 'Retail and food services sales, total';
+
+-- Yearly trend of total Retail and food services sales
+select date_part('year', sales_month) as sales_year, sum(sales) as sales
+from retail_sales
+where kind_of_business = 'Retail and food services sales, total'
+group by 1
+order by 1 asc
